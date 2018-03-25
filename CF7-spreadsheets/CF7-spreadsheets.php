a525bf96a539751255e5bdf4f4290eb6fe6fb02c
moshensky <moshensky.c1369@ya.ru>
2018-03-25 13:56:20 +0300

    v 2.0.3 version update

--- CF7-spreadsheets/CF7-spreadsheets.php
+++ CF7-spreadsheets/CF7-spreadsheets.php
@@ -3,7 +3,7 @@
 Plugin Name: CF7 Spreadsheets
 Plugin URI: https://github.com/moshenskyDV/CF7-spreadsheets
 Description: Send Contact form 7 mail to Google spreadsheets
-Version: 2.0.2
+Version: 2.0.3
 Author: Moshenskyi Danylo
 Author URI: https://github.com/moshenskyDV/
 Text Domain: CF7-spreadsheets
--- CF7-spreadsheets/readme.txt
+++ CF7-spreadsheets/readme.txt
@@ -3,7 +3,7 @@ Contributors: Moshenskyi Danylo
 Tags: contact form 7, google, spreadsheets, table, data, merge, save mail
 Requires at least: 4.7
 Tested up to: 4.7
-Stable tag: 2.0.0
+Stable tag: 2.0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
